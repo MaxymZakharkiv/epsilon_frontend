28b38313b7a2760004a73a774332d457c39f5e91
Maksym <mzaharkiv77@gmail.com>
2022-05-18 12:42:00 +0300

    second commit

--- epsilon_frontend/src/components/Table.vue
+++ epsilon_frontend/src/components/Table.vue
@@ -1,8 +1,31 @@
 <template>
   <h3>Table</h3>
-  <p v-for="i in store.data" :key="i.id">
-    {{ i }}
-  </p>
+  <q-table
+    :title="title"
+    :rows="store.data"
+    :columns="columns"
+  >
+    <template v-slot:body-cell-remove="props">
+      <q-td :props="props">
+        <q-btn
+          color="negative"
+          icon-right="delete"
+          no-caps
+          flat
+          dense
+        />
+      </q-td>
+    </template>
+    <template v-slot:no-data="{ icon, message, filter }">
+      <div class="full-width row flex-center text-accent q-gutter-sm">
+        <q-icon size="2em" name="sentiment_dissatisfied" />
+        <span>
+            Немає даних
+          </span>
+        <q-icon size="2em" :name="filter ? 'filter_b_and_w' : icon" />
+      </div>
+    </template>
+  </q-table>
 </template>
 
 <script>
@@ -13,6 +36,12 @@ export default {
   props:{
     api:{
       required: true
+    },
+    columns:{
+      required: true
+    },
+    title:{
+      required:true
     }
   },
   setup(props){
--- epsilon_frontend/src/pages/Directives/Region.vue
+++ epsilon_frontend/src/pages/Directives/Region.vue
@@ -1,5 +1,9 @@
 <template>
-  <Table :api="api"/>
+  <Table
+    :title="'Райони'"
+    :api="api"
+    :columns="columns"
+  />
 </template>
 
 <script>
@@ -11,8 +15,50 @@ export default {
     Table
   },
   setup(){
+
+    const columns = [
+      {
+        name:'id',
+        label:'id',
+        align:'left',
+        sortable:true,
+        field: row => row.id,
+        format:val => `${val}`
+      },
+      {
+        name:'schema',
+        label:'schema',
+        align:'left',
+        sortable:true,
+        field: row => row.schema,
+        format:val => `${val}`
+      },
+      {
+        name:'name',
+        label:'name',
+        align:'left',
+        sortable:true,
+        field: row => row.name,
+        format:val => `${val}`
+      },
+      {
+        name:'name_aliases',
+        label:'name_aliases',
+        align:'left',
+        sortable:true,
+        field: row => row.name_aliases,
+        format: (val) => val.join(', ')
+      },
+      {
+        name:'remove',
+        label:'remove',
+        align:'left'
+      },
+    ]
+
     return{
-      api
+      api,
+      columns
     }
   }
 }
